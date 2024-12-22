8234260a10a7e4a433a274c1b0e7b65502d75bec
Diego Torres <tdiego001@gmail.com>
2024-12-22 16:47:48 -0700

    WW-1 Added accessors and mutators to star class

--- code/src/star.cpp
+++ code/src/star.cpp
@@ -29,6 +29,7 @@
 ** 2.  INCLUDE FILES
 **===============================================================================================*/
 #include "worldweaver/star.hpp"
+#include <cstring>
 
 /*=================================================================================================
 ** 3.  DECLARATIONS
@@ -60,8 +61,238 @@
 /**
 * \par Details: 
 */
-WorldWeaver::Model::Star::Star()
+WorldWeaver::Model::Star::Star() :
+    m_Mass(0.0),
+    m_CurrentAge(0.0),
+    m_MaxAge(0.0),
+    m_Radius(0.0),
+    m_Luminosity(1.0),
+    m_Density(1.0),
+    m_Temperature(1.0),
+    m_Color(1.0),
+    m_MinHabitableZone(0.0),
+    m_MaxHabitableZone(0.0),
+    m_IsEarthLike(false)
 {
+    m_SpectralClass.spectralMajor = SpectralMajor::UNSET;
+    m_SpectralClass.spectralMinor = 0.0;
+    m_SpectralClass.isMainSequence = false;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetSpectralClass(SpectralClass spectralClass)
+{
+    m_SpectralClass = spectralClass;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetMass(double mass)
+{
+    m_Mass = mass;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetCurrentAge(double currentAge)
+{
+    m_CurrentAge = currentAge;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetMaxAge(double maxAge)
+{
+    m_MaxAge = maxAge;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetRadius(double radius)
+{
+    m_Radius = radius;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetLuminosity(double luminosity)
+{
+    m_Luminosity = luminosity;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetDensity(double density)
+{
+    m_Density = density;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetTemperature(double temperature)
+{
+    m_Temperature = temperature;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetColor(double color)
+{
+    m_Color = color;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetMinHabitableZone(double minHabitableZone)
+{
+    m_MinHabitableZone = minHabitableZone;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetMaxHabitableZone(double maxHabitableZone)
+{
+    m_MaxHabitableZone = maxHabitableZone;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+void WorldWeaver::Model::Star::SetIsEarthLike(bool isEarthLike)
+{
+    m_IsEarthLike = isEarthLike;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+WorldWeaver::Model::Star::SpectralClass WorldWeaver::Model::Star::GetSpectralClass() const
+{
+    return m_SpectralClass;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetMass() const
+{
+    return m_Mass;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetCurrentAge() const
+{
+    return m_CurrentAge;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetMaxAge() const
+{
+    return m_MaxAge;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetRadius() const
+{
+    return m_Radius;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetLuminosity() const
+{
+    return m_Luminosity;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetDensity() const
+{
+    return m_Density;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetTemperature() const
+{
+    return m_Temperature;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetColor() const
+{
+    return m_Color;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetMinHabitableZone() const
+{
+    return m_MinHabitableZone;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+double WorldWeaver::Model::Star::GetMaxHabitableZone() const
+{
+    return m_MaxHabitableZone;
+}
+
+/**************************************************************************************************/
+/**
+ * \par Details: 
+ */
+bool WorldWeaver::Model::Star::GetIsEarthLike() const
+{
+    return m_IsEarthLike;
 }
 
 /*=================================================================================================
